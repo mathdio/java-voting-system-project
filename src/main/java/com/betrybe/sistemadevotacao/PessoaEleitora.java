5b300095d65657d94db8455fcb6deac9abde0c16
Matheus Diógenes <mathdiogenes@gmail.com>
2023-11-06 15:27:54 -0300

    feat: R3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,20 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa eleitora.
  */
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
